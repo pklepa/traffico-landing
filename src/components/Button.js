cd78fbfcb66859de89ee4a34a7a75f8c219b2b47
Pedro Klepa <pedro.klepa@gmail.com>
2021-01-13 15:01:55 +0000

    Add animated button to hero section

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,24 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+import rightArrow from "../assets/images/RightArrow.svg";
+
+const bounce = keyframes`
+  0%, 100% {
+    right: 10px;
+  }
+  50% {
+    right: 25px;
+  }
+
+`;
 
 export const Button = styled.button`
+  position: relative;
+  display: flex;
+  align-items: center;
+
   background-color: ${(props) => props.theme.colors.red};
   color: white;
-  padding: 20px 24px;
+  padding: 20px ${(props) => (props.callToAction ? "80px 20px 24px" : "24px")};
   border: none;
   border-radius: 5px;
   text-transform: uppercase;
@@ -12,4 +27,18 @@ export const Button = styled.button`
 
   letter-spacing: 1px;
   cursor: pointer;
+
+  &::after {
+    display: ${(props) => (props.callToAction ? "block" : "none")};
+    content: "";
+    position: absolute;
+    width: 40px;
+    height: 16px;
+
+    animation: ${bounce} 1.3s ease-in-out infinite;
+
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-image: url(${rightArrow});
+  }
 `;
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,16 +1,39 @@
 import React from "react";
 import styled from "styled-components";
 import HeroImg from "../assets/images/MainScene.png";
+import { Button } from "./Button";
 
 function HeroSection() {
   return (
-    <section>
+    <Container>
       <SceneImg src={HeroImg} alt="" />
-    </section>
+
+      <h1>Your awesome traffic permit consultant.</h1>
+      <Button callToAction>Get started</Button>
+    </Container>
   );
 }
 
 export default HeroSection;
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  h1 {
+    font-size: 100px;
+    font-family: ${(props) => props.theme.fonts.title};
+    font-weight: normal;
+    color: white;
+    line-height: 1em;
+
+    width: 740px;
+
+    margin-top: 90px;
+    margin-bottom: 60px;
+  }
+`;
 
 const SceneImg = styled.img`
   position: absolute;
